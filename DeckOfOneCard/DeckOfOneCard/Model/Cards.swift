f585d4eb34917d1c9307a3b49a40b68c48578c71
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-06-24 11:13:38 -0600

    finished card model

---
+++ DeckOfOneCard/DeckOfOneCard/Model/Cards.swift
@@ -0,0 +1,24 @@
+//
+//  Cards.swift
+//  DeckOfOneCard
+//
+//  Created by Madison Kaori Shino on 6/24/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+import Foundation
+
+class Card: Codable {
+    
+    let suit: String
+    let value: String
+    let code: String
+    let image: String
+    
+    init(suit: String, value: String, code: String, image: String) {
+        self.suit = suit
+        self.value = value
+        self.code = code
+        self.image = image
+    }
+}
